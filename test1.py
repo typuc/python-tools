f7c9c7bb56698889e675fcd16f3d80feb0b00da0
谢荣飞 <xierongfei@haixue.com>
2022-03-25 18:00:49 +0800

    source

--- test1.py
+++
@@ -1,7 +0,0 @@
-import xml.etree.ElementTree as ET
-import json
-import re
-import requests
-import requests
-r=requests.post(url='http://ip.taobao.com/service/getIpInfo2.php', data={'ip': '223.104.192.174'})
-print(r.json())
